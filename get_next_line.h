ddf16535dc31ddd69d9d2c7321af94813e21ff00
Frederic Leonard <fleonard@z4r7p3.le-101.fr>
2018-10-25 20:48:50 +0200

    get_next_line by Lettoh

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   get_next_line.h                                  .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: fleonard <fleonard@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/10/15 14:46:07 by fleonard     #+#   ##    ##    #+#       */
+/*   Updated: 2018/10/17 13:47:58 by fleonard    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 42
+
+# include <unistd.h>
+# include <stdlib.h>
+# include "./libft/libft.h"
+
+int		get_next_line(const int fd, char **line);
+
+#endif
